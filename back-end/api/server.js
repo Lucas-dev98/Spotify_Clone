d52aa46c4d3d92bed2be4c69e0064e9176336244
lucas-dev98 <l.o.bastos@live.com>
2025-05-26 10:30:20 -0300

    Feat : Adding Player Funcions

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,23 +1,31 @@
-//API
-import express from 'express';
-import {artistArray} from "../../front-end/src/assets/database/artists.js"
-import {songsArray} from "../../front-end/src/assets/database//songs.js";
+// API significa Application Programming Interface
+// POST, GET, PUT, DELETE
+// CRUD - Create Read Update Delete
+// Endpoint
+// Middleware
+
+import express from "express";
+import cors from "cors";
+import { db } from "./connect.js";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
+// app.use(express.json());
+
 app.get("/", (request, response) => {
-    response.send("So vamos trabalhar com os endpoints /artists e /songs");
+  response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", (request, response) => {
-    response.send(artistArray);
+app.get("/artists", async (request, response) => {
+  response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", (request, response) => {
-    response.send("songArray");
+app.get("/songs", async (request, response) => {
+  response.send(await db.collection("songs").find({}).toArray());
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Servidor está escutando na porta ${PORT}`);
 });
